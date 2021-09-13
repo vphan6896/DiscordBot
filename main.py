d5b48d698e0d9cdfea9e7e21fc27e19fc0d91180
vphan6896 <vp6896@utexas.edu>
2021-09-13 17:22:06 -0500

    Logging title on ytdl search. Now can output downloads into a separate directory instead of root directory

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import discord
 from discord.ext import commands, tasks
 import youtube_dl
+import sys
 
 secretFile = open("discordSecret", "r")
 clientID = secretFile.readline()
@@ -18,6 +19,7 @@ bot = commands.Bot(command_prefix='?', self_deaf=True)
 
 youtube_dl.utils.bug_reports_message = lambda: ''
 ytdl_format_options = {
+    'outtmpl': './playlist/%(title)s-%(id)s.%(ext)s',
     'format': 'bestaudio/best',
     'restrictfilenames': True,
     'noplaylist': True,
@@ -39,17 +41,19 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.data = data
         self.title = data.get('title')
         self.url = ""
+
     @classmethod
     async def from_url(cls, url, *, loop=None, stream=False):
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
         if 'entries' in data:
+            count = 0
             for i in data['entries']:
                 print("Entry {}: {}".format(count, i['title']))
-                count += 1
+                count = count + 1
             # take first item from a playlist
             data = data['entries'][0]
-            count = 0
+            
             
         filename = data['title'] if stream else ytdl.prepare_filename(data)
         return filename
@@ -57,7 +61,8 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.CustomActivity(name="Vybing"))
+    act=discord.Activity(name="Vybing")
+    await bot.change_presence(activity=act)
     print("The bot is ready!")
 
 @bot.command()
@@ -93,10 +98,14 @@ async def play(ctx,url):
             filename = await YTDLSource.from_url(url, loop=bot.loop)
             voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
         #Remove file extension from message
-        friendlyFileName = filename.split('.')
-        await ctx.send('**Now playing:** {}'.format(friendlyFileName[0]))
+        friendlyFileName = filename.strip("playlist\")
+        friendlyFileName = friendlyFileName.split('.')[0]
+        friendlyFileName = friendlyFileName.replace("_"," ")
+        await ctx.send('**Now playing:** {}'.format(friendlyFileName))
     except:
         await ctx.send("Playing: The bot is not connected to a voice channel.")
+        e = sys.exc_info()[0]
+        print("Error: %s" % e)
 
 
 @bot.command(name='pause', help='This command pauses the song')
@@ -119,7 +128,7 @@ async def resume(ctx):
 async def stop(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_playing():
-        await voice_client.stop()
+        voice_client.stop()
     else:
         await ctx.send("Stopping: The bot is not playing anything at the moment.")
 
